7e91a1d5f4ffb78ad8668ee11ca8d5841f21b677
Filip <filip.malczak@gmail.com>
2020-04-08 00:02:36 +0200

    Dirty, but working fix for circular dependencies

--- source/dejector.d
+++ source/dejector.d
@@ -10,158 +10,190 @@ extern (C) Object _d_newclass(const TypeInfo_Class ci);
 private immutable argumentSeparator = ", ";
 
 private string generateGet(T)() {
-	immutable nameOfT = fullyQualifiedName!T;
-	auto code = "
-		Object get() {
-			auto instance = cast(T) _d_newclass(T.classinfo);";
+    immutable nameOfT = fullyQualifiedName!T;
+    auto code = "
+        Object get(string key, ref Object[string] known) {
+            auto instance = cast(T) _d_newclass(T.classinfo);
+            known[key] = instance;";
 
 
-	static if (hasMember!(T, "__ctor")) {
-		foreach (type; ParameterTypeTuple!(T.__ctor)) {
-			code ~= "import " ~ moduleName!type ~ ";";
-		}
+    static if (hasMember!(T, "__ctor")) {
+        foreach (type; ParameterTypeTuple!(T.__ctor)) {
+            code ~= "import " ~ moduleName!type ~ ";";
+        }
 
-		code ~= "instance.__ctor(";
+        code ~= "instance.__ctor(";
 
-		foreach (type; ParameterTypeTuple!(T.__ctor)) {
-			code ~= "this.dej.get!(" ~ fullyQualifiedName!type ~ ")" ~
-				argumentSeparator;
-		}
-		code = chomp(code, argumentSeparator) ~ ");";
-	}
-	code ~= "return instance; }";
-	return code;
+        foreach (type; ParameterTypeTuple!(T.__ctor)) {
+            code ~= "this.dej.get!(" ~ fullyQualifiedName!type ~ ")(known)" ~
+                argumentSeparator;
+        }
+        code = chomp(code, argumentSeparator) ~ ");";
+    }
+    code ~= "return instance; }";
+    return code;
 }
 
 
 interface Provider {
-	Object get();
+    Object get(string key, ref Object[string] known);
 }
 
 
 class ClassProvider(T) : Provider {
-	private Dejector dej;
-	this(Dejector dejector) {
-		this.dej = dejector;
-	}
-	mixin(generateGet!T);
+    private Dejector dej;
+    this(Dejector dejector) {
+        this.dej = dejector;
+    }
+    mixin(generateGet!T);
 }
 
 
 class FunctionProvider : Provider {
-	private Object delegate() provide;
+    private Object delegate(ref Object[string]) provide;
 
-	this(Object delegate() provide) {
-		this.provide = provide;
-	}
+    this(Object delegate(ref Object[string]) provide) {
+        this.provide = provide;
+    }
 
-	Object get() {
-		return this.provide();
-	}
+    Object get(string key, ref Object[string] known) {
+        auto v = this.provide(known);
+        known[key] = v;
+        return v;
+    }
 }
 
 
 class InstanceProvider : Provider {
-	private Object instance;
+    private Object instance;
 
-	this(Object instance) {
-		this.instance = instance;
-	}
+    this(Object instance) {
+        this.instance = instance;
+    }
 
-	Object get() {
-		return this.instance;
-	}
+    Object get(string key, ref Object[string] known) {
+        known[key] = this.instance;
+        return this.instance;
+    }
 }
 
 
 private struct Binding {
-	string key;
-	Provider provider;
-	Scope scope_;
+    string key;
+    Provider provider;
+    Scope scope_;
 }
 
 
 interface Scope {
-	Object get(string key, Provider provider);
+    Object get(string key, Provider provider, ref Object[string] known);
 }
 
-
+//todo IMO it's Prototype
 class NoScope : Scope {
-	Object get(string key, Provider provider) {
-		return provider.get;
-	}
+    Object get(string key, Provider provider, ref Object[string] known) {
+        import std.stdio;
+        import std.conv;
+        writeln("NoScope key "~key~" "~to!string(known), key);
+        auto o = provider.get(key, known);
+        writeln("RETURN NEW ", to!string(&o), key);
+        return o;
+    }
 }
 
 class Singleton : Scope {
-	private Object[string] instances;
+    private Object[string] instances;
 
-	Object get(string key, Provider provider) {
-		if(key !in this.instances) {
-			this.instances[key] = provider.get;
-		}
-		return this.instances[key];
-	}
+    Object get(string key, Provider provider, ref Object[string] known) {
+        import std.stdio;
+        writeln("Singleton key "~key~" "~to!string(known));
+        auto found = key in known;
+        if (found){
+            writeln("ALREADY KNOWN ", key);
+            return known[key];
+        }
+        if(key !in this.instances) {
+            this.instances[key] = provider.get(key, known);
+        }
+        return this.instances[key];
+    }
 }
 
 
 interface Module {
-	void configure(Dejector dejector);
+    void configure(Dejector dejector);
 }
 
 
 class Dejector {
-	private Binding[string] bindings;
-	private Scope[string] scopes;
+    private Binding[string] bindings;
+    private Scope[string] scopes;
 
-	this(Module[] modules) {
-		this.bindScope!NoScope;
-		this.bindScope!Singleton;
+    this(Module[] modules) {
+        this.bindScope!NoScope;
+        this.bindScope!Singleton;
 
-		foreach(module_; modules) {
-			module_.configure(this);
-		}
-	}
+        foreach(module_; modules) {
+            module_.configure(this);
+        }
+    }
 
-	this() {
-		this([]);
-	}
+    this() {
+        this([]);
+    }
 
-	void bindScope(Class)() {
-		immutable key = fullyQualifiedName!Class;
-		if(key in this.scopes) {
-			throw new Exception("Scope already bound");
-		}
-		this.scopes[key] = new Class();
-	}
+    void bindScope(Class)() {
+        immutable key = fullyQualifiedName!Class;
+        if(key in this.scopes) {
+            throw new Exception("Scope already bound");
+        }
+        this.scopes[key] = new Class();
+    }
 
-	void bind(Class, ScopeClass:Scope = NoScope)() {
-		this.bind!(Class, Class, ScopeClass);
-	}
+    void bind(Class, ScopeClass:Scope = NoScope)() {
+        this.bind!(Class, Class, ScopeClass);
+    }
 
-	void bind(Interface, Class, ScopeClass:Scope = NoScope)() {
-		this.bind!(Interface, ScopeClass)(new ClassProvider!Class(this));
-	}
+    void bind(Interface, Class, ScopeClass:Scope = NoScope)() {
+        this.bind!(Interface, ScopeClass)(new ClassProvider!Class(this));
+    }
 
-	void bind(Interface, ScopeClass:Scope = NoScope)(Provider provider) {
-		immutable key = fullyQualifiedName!Interface;
-		if(key in this.bindings) {
-			throw new Exception("Interface already bound");
-		}
-		auto scope_ = this.scopes[fullyQualifiedName!ScopeClass];
-		this.bindings[key] = Binding(key, provider, scope_);
-	}
+    void bind(Interface, ScopeClass:Scope = NoScope)(Provider provider) {
+        immutable key = fullyQualifiedName!Interface;
+        if(key in this.bindings) {
+            throw new Exception("Interface already bound");
+        }
+        auto scope_ = this.scopes[fullyQualifiedName!ScopeClass];
+        this.bindings[key] = Binding(key, provider, scope_);
+    }
 
-	void bind(Interface, ScopeClass:Scope = NoScope)(Object delegate() provide) {
-		this.bind!(Interface, ScopeClass)(new FunctionProvider(provide));
-	}
+    void bind(Interface, ScopeClass:Scope = NoScope)(Object delegate(ref Object[string]) provide) {
+        this.bind!(Interface, ScopeClass)(new FunctionProvider(provide));
+    }
 
-	void bind(Interface, ScopeClass:Scope = NoScope)(Object function() provide) {
-		this.bind!(Interface, ScopeClass)(toDelegate(provide));
-	}
+    void bind(Interface, ScopeClass:Scope = NoScope)(Object function(ref Object[string]) provide) {
+        this.bind!(Interface, ScopeClass)(toDelegate(provide));
+    }
+    
+    private static Object[string] emptyKnown;
 
-	Interface get(Interface)() {
-		auto binding = this.bindings[fullyQualifiedName!Interface];
-		immutable key = fullyQualifiedName!Interface;
-		return cast(Interface) binding.scope_.get(key, binding.provider);
-	}
+    Interface get(Interface)() {
+        Object[string] known;
+        return get!(Interface)(known);
+    }
+
+    Interface get(Interface)(ref Object[string] known) {
+        return get!(Interface, Interface)(known);
+    }
+
+    Interface get(Query, Interface)() {
+        Object[string] known;
+        return get!(Query, Interface)(known);
+    }
+
+    Interface get(Query, Interface)(ref Object[string] known) {
+        auto binding = this.bindings[fullyQualifiedName!Query];
+        immutable key = fullyQualifiedName!Query;
+        return cast(Interface) binding.scope_.get(key, binding.provider, known);
+    }
 }
--- source/dejector_tests.d
+++ source/dejector_tests.d
@@ -1,62 +1,112 @@
 import dejector : Dejector, InstanceProvider, Module, NoScope, Singleton;
 
 version(unittest) {
-	class X {}
+    class X {}
 
-	class User {
-		string name;
-		this(string name) {
-			this.name = name;
-		}
-	}
+    class User {
+        string name;
+        this(string name) {
+            this.name = name;
+        }
+    }
 
-	interface Greeter {
-		string greet();
-	}
+    interface Greeter {
+        string greet();
+    }
 
-	class GreeterImplementation : Greeter {
-		this(X x) {}
-		string greet() { return "Hello!"; }
-	}
+    class GreeterImplementation : Greeter {
+        this(X x) {}
+        string greet() { return "Hello!"; }
+    }
 
-	unittest {
-		class MyModule : Module {
-			void configure(Dejector dejector) {
-				dejector.bind!(X);
-				dejector.bind!(Greeter, GreeterImplementation);
-				dejector.bind!(User)(function() { return new User("root"); });
-			}
-		}
+    unittest {
+        class MyModule : Module {
+            void configure(Dejector dejector) {
+                dejector.bind!(X);
+                dejector.bind!(Greeter, GreeterImplementation);
+                dejector.bind!(User)(function(ref Object[string] k) { return new User("root"); });
+            }
+        }
 
-		auto dejector = new Dejector([new MyModule()]);
+        auto dejector = new Dejector([new MyModule()]);
 
-		auto greeter = dejector.get!Greeter;
-		assert(greeter.greet == "Hello!");
+        auto greeter = dejector.get!Greeter;
+        assert(greeter.greet == "Hello!");
 
-		auto user = dejector.get!User;
-		assert(user.name == "root");
-	}
+        auto user = dejector.get!User;
+        assert(user.name == "root");
+    }
 
-	/// InstanceProvider works
-	unittest {
-		auto dejector = new Dejector;
-		dejector.bind!(User)(new InstanceProvider(new User("Jon")));
-		assert(dejector.get!User.name == "Jon");
-	}
+    /// InstanceProvider works
+    unittest {
+        auto dejector = new Dejector;
+        dejector.bind!(User)(new InstanceProvider(new User("Jon")));
+        assert(dejector.get!User.name == "Jon");
+    }
 
-	/// NoScope binding creates new object on every call
-	unittest {
-		auto dejector = new Dejector;
-		dejector.bind!(X, NoScope);
+    /// NoScope binding creates new object on every call
+    unittest {
+        auto dejector = new Dejector;
+        dejector.bind!(X, NoScope);
 
-		assert(dejector.get!X() !is dejector.get!X());
-	}
+        assert(dejector.get!X() !is dejector.get!X());
+    }
 
-	/// Singleton binding always returns the same object
-	unittest {
-		auto dejector = new Dejector;
-		dejector.bind!(X, Singleton);
+    /// Singleton binding always returns the same object
+    unittest {
+        auto dejector = new Dejector;
+        dejector.bind!(X, Singleton);
 
-		assert(dejector.get!X is dejector.get!X);
-	}
+        assert(dejector.get!X is dejector.get!X);
+    }
+}
+
+///Circular dependencies
+version(unittest){
+    import std.conv;
+
+    class A {
+        B _b;
+        
+        this(B b){
+            _b = b;
+        }
+        
+        override string toString(){
+            return "A(b@"~to!string(&_b)~")";
+        }
+    }
+
+    class B {
+        A _a;
+        
+        this(A a){
+            _a = a;
+        }
+        
+        override string toString(){
+            return "B(a@"~to!string(&_a)~")";
+        }
+    }
+
+    class CircularModule : Module {
+        void configure(Dejector dejector) {
+            dejector.bind!(A, Singleton);
+            dejector.bind!(B, Singleton);
+        }
+    }
+
+    unittest {
+        auto dejector = new Dejector([new CircularModule()]);
+
+        auto a = dejector.get!A;
+        import std.stdio;
+        writeln(a, "@"~to!string(&a));
+        writeln(a._b, "@"~to!string(&(a._b)));
+        writeln(a._b._a);
+        assert(a !is null);
+        assert(a._b !is null);
+        assert(a._b._a !is null);
+        assert(a._b._a is a); //todo check comparison
+    }
 }
